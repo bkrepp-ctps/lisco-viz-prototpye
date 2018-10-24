12630d1533b2ef6e4c9186c068e87fb308f478b2
Ben Krepp <bkrepp@ctps.org>
2018-10-24 13:53:20 -0400

    Implemented support for change of symbology.

--- balanced_volumes_viz.js
+++ balanced_volumes_viz.js
@@ -1,41 +1,53 @@
 $(document).ready(function() {
     initialize();
-    // The following folderol is necessary because of the way jQueryUI works...
+    // The following folderol is necessary because of the way the jQueryUI accordion control works...
     $('#i93_sb_accordion').show();
     var activePanel = $('#i93_nb_accordion').accordion('option', 'active');
     prepDataAndGenerateViz('i93_sb', activePanel);
 });	
 
+// Data structure encapsulating the state of all accordion panels
+// If chartApi == null, "panel" has not been initialized
 var panelsState = {
-    'i93_nb' :  {   'panels' :  [   { name: 'xx', panelIx : 0, initialized: false },
-                                    { name: 'xx', panelIx : 1, initialized: false },
-                                    { name: 'xx', panelIx : 2, initialized: false },
-                                    { name: 'xx', panelIx : 3, initialized: false },
-                                    { name: 'xx', panelIx : 4, initialized: false },
-                                    { name: 'xx', panelIx : 5, initialized: false }  
+    'i93_nb' :  {   'panels' :  [   { name: 'xx', panelIx : 0, chartApi : null },
+                                    { name: 'xx', panelIx : 1, chartApi : null },
+                                    { name: 'xx', panelIx : 2, chartApi : null },
+                                    { name: 'xx', panelIx : 3, chartApi : null },
+                                    { name: 'xx', panelIx : 4, chartApi : null },
+                                    { name: 'xx', panelIx : 5, chartApi : null }  
                                 ]
                 },   
-    'i93_sb' :  {   'panels' :  [   { name: 'i93_sb_wilmington',            panelIx:  0, initialized: false },
-                                    { name: 'i93_sb_woburn_to_stoneham',    panelIx : 1, initialized: false },
-                                    { name: 'i93_sb_medford_to_somerville', panelIx : 2, initialized: false },
-                                    { name: 'i93_sb_storrow_to_kneeland',   panelIx : 3, initialized: false },
-                                    { name: 'i93_sb_kneeland_to_columbia',  panelIx : 4, initialized: false },
-                                    { name: 'i93_sb_freeport_to_braintree', panelIx : 5, initialized: false }   
+    'i93_sb' :  {   'panels' :  [   { name: 'i93_sb_wilmington',            panelIx:  0, chartApi : null },
+                                    { name: 'i93_sb_woburn_to_stoneham',    panelIx : 1, chartApi : null },
+                                    { name: 'i93_sb_medford_to_somerville', panelIx : 2, chartApi : null },
+                                    { name: 'i93_sb_storrow_to_kneeland',   panelIx : 3, chartApi : null },
+                                    { name: 'i93_sb_kneeland_to_columbia',  panelIx : 4, chartApi : null },
+                                    { name: 'i93_sb_freeport_to_braintree', panelIx : 5, chartApi : null }   
                                 ]
                 },
 
-    'sr3_nb' :  {   'panels' : [    { name: 'sr3_nb', panelIx : 0, initialized: false }
+    'sr3_nb' :  {   'panels' : [    { name: 'sr3_nb', panelIx : 0, chartApi : null }
                                ]                
                 },
-    'sr3_sb' :  {   'panels' : [    { name: 'sr3_sb', panelIx : 0, initialized: false }
+    'sr3_sb' :  {   'panels' : [    { name: 'sr3_sb', panelIx : 0, chartApi: null }
                                ]
                 }
 };
+
+var volumePaletteColor = d3.scaleThreshold()
+                    .domain([0, 25000, 50000, 75000, 100000, 150000])
+                    .range(["gray", "blue", "green", "yellow", "orange", "red"]);
+                    
+var volumePaletteWidth = d3.scaleThreshold()
+                     .domain([0, 25000, 50000, 75000, 100000, 150000])
+                     .range(["1px", "2px", "3px", "4px", "5px", "6px"]);   
 
 function initialize() {
     $('.accordion').accordion({ heightStyle: 'content' });
     $('.accordion').hide();
     $('#select_route').selectmenu();
+    
+    // on-change event handler for select_route <select> control
     $('#select_route').on( "selectmenuchange", 
         function(event, ui) {
             $('.accordion').hide();
@@ -46,40 +58,39 @@ function initialize() {
             // which accordion panel is being activated, i.e., wihch route segment we're dealing with.
             // If viz not yet generated, generate it.
             var activePanel = $('#' + accordion_id).accordion('option', 'active');
-            if (panelsState[selected_route_id].panels[activePanel].initialized == false) {
+            if (panelsState[selected_route_id].panels[activePanel].chartApi == null) {
                 prepDataAndGenerateViz(selected_route_id, activePanel);
             }            
         });
-/*   
-    $('#select_route').bind('change', 
-        function(event) {
-            $('.accordion').hide();
-            var selected_route_id = $('#select_route option:selected').val();
-            var accordion_id = selected_route_id + '_accordion';
-            $('#' + accordion_id).show();
-            // Determine which accordion is active, i.e., which route we're dealing with, and
-            // which accordion panel is being activated, i.e., wihch route segment we're dealing with.
-            // If viz not yet generated, generate it.
-            var activePanel = $('#' + accordion_id).accordion('option', 'active');
-            if (panelsState[selected_route_id].panels[activePanel].initialized == false) {
-                prepDataAndGenerateViz(selected_route_id, activePanel);
-            }
-        });
- */
+
+    // on-activate event handler for accordion panels
     $( ".accordion" ).on('accordionactivate', 
         function(event, ui) {
             // Determine which accordion is active, i.e., which route we're dealing with, and
             // which accordion panel is being activated, i.e., wihch route segment we're dealing with.
             // If viz not yet generated, generate it.
             var routeStr = event.target.id.replace('_accordion', '');
-            var segNum = +ui.newPanel[0].id.replace(routeStr + '_viz_','');
-            if (panelsState[routeStr].panels[segNum].initialized == false) {
-                prepDataAndGenerateViz(routeStr, segNum);
+            var panelNum = +ui.newPanel[0].id.replace(routeStr + '_viz_','');
+            if (panelsState[routeStr].panels[panelNum].chartApi == null) {
+                prepDataAndGenerateViz(routeStr, panelNum);
             }
         });
+    
+    // on-change event handler for select_metric <select> controls
+    $('.select_metric').on('change', function(event) {
+        // 'this' is the HTML <select> element that triggered the event
+        var metric = this.selectedOptions[0].getAttribute('metric');
+        var routeStr = this.parentElement.parentElement.id.replace('_accordion','');
+        var panelNum = +this.parentElement.id.replace(routeStr + '_viz_', '');
+        // console.log('route : ' + routeStr + ' panel : ' + panelNum + ' metric : ' + metric);
+        symbolizeViz(routeStr, panelNum, metric);
+    });
 } // initialize()
 
+// prepDataAndGenerateViz - function
+//
 function prepDataAndGenerateViz(routeName, panelIx) {
+    var chartApi = {};
     var csvName = 'data/' + panelsState[routeName].panels[panelIx].name + '.csv';
     var volumesName = 'data/' + panelsState[routeName].panels[panelIx].name +  '_VOLUMES.csv';
     var q = d3.queue()
@@ -122,27 +133,33 @@ function prepDataAndGenerateViz(routeName, panelIx) {
                     i.volumes.peak_2010_5_to_6_pm  = +i.volumes.peak_2010_5_to_6_pm;
                     i.volumes.peak_2010_6_to_7_pm  = +i.volumes.peak_2010_6_to_7_pm;
                 });
-                generateViz(routeName, panelIx, lineData);
+                chartApi = generateViz(routeName, panelIx, lineData);
+                panelsState[routeName].panels[panelIx].chartApi = chartApi;
+                // When first created, default synbolization is by 2010 AWDT
+                symbolizeViz(routeName, panelIx, 'awdt_2010');
             });
-            panelsState[routeName].panels[panelIx].initialized = true;
-} // prepDataAnd GenerateViz()
+} // prepDataAndGenerateViz()
 
-            
+// generateViz - function
+//
+// Given:
+//       (1) a route name string, 
+//       (2) a panel index number, and 
+//       (3) CSV data definline the SVG linework for the route
+//           setment and the associated balanced volume data,
+// does the following:
+//       (1) generates the SVG <line> elements for a "wireframe" chart and
+//           SVG <text> elements for the associated data labels
+//       (2) creates an object representing the API to these selected SVG elements
+//           bound to the corresponding volume data, and stores this in the 
+//          'chartApi' property of the relevant object in 'panelsState'  
 function generateViz(routeName, panelIx, lineData) {
     // Define SVG container ('viewport')
     var height = 300; // Hardwired for now
     /* var width = 3550;  */
     var width = d3.max([d3.max(lineData, function(d) { return d.x1; }),
                         d3.max(lineData, function(d) { return d.x2; })]);
-    
-    var volumePaletteColor = d3.scaleThreshold()
-                        .domain([0, 25000, 50000, 75000, 100000, 150000])
-                        .range(["gray", "blue", "green", "yellow", "orange", "red"]);
-                        
-    var volumePaletteWidth = d3.scaleThreshold()
-                         .domain([0, 25000, 50000, 75000, 100000, 150000])
-                         .range(["1px", "2px", "3px", "4px", "5px", "6px"]);   
- 
+
     var divName = routeName + '_viz_' + panelIx;
 		
 	var svgContainer = d3.select('#' + divName).append("svg")
@@ -160,11 +177,7 @@ function generateViz(routeName, panelIx, lineData) {
             .attr("y1", function(d, i) { return d.y1; })
             .attr("x2", function(d, i) { return d.x2; })
             .attr("y2", function(d, i) { return d.y2; })
-            .attr("class", function(d, i) { return d.type; })
-			.style("stroke", function(d, i) { return volumePaletteColor(d.volumes.awdt_2010); }) 
-            // .style("stroke", function(d, i) { return volumePalette(d.volumes.peak_2010_6_to_7_am); })
-			.style("stroke-width", function(d, i) { return volumePaletteWidth(d.volumes.awdt_2010); })
-			.on("click", function(d, i) { consle.log(d.unique_id); });
+            .attr("class", function(d, i) { return d.type; });
     
     var svgText = svgContainer.selectAll("text")
         .data(lineData)
@@ -172,10 +185,26 @@ function generateViz(routeName, panelIx, lineData) {
         .append("text")
             .attr("id", function(d, i) { return 'txt_' +d.unique_id; })
             .attr("class", function(d, i) { return 'txt_' + d.type; })
-            .text(function(d, i) { return d.volumes.awdt_2010.toLocaleString(); })
             .attr("x", function(d, i) { return d.x1 + ((d.x2 - d.x1)/2); })
             .attr("y", function(d, i) { return d.y1 + ((d.y2 - d.y1)/2) - 5; })
-            .attr("text-anchor", "middle");
+            .attr("text-anchor", "middle")
+            .text('');
             
-    var _DEBUG_HOOK = 0;
+    var chartApi = {
+        lines : svgRouteSegs,
+        txt   : svgText
+    };      
+    return chartApi;
 } // generateViz()
+
+// synbolizeViz - function
+// Symbolize the chart in the panelNum-th panel of the accordion display for "routeStr",
+// according to the selected metric
+function symbolizeViz(routeStr, panelNum, metric) {
+    var chartApi = panelsState[routeStr].panels[panelNum].chartApi;
+    chartApi.lines
+        .style("stroke", function(d, i) { return volumePaletteColor(d.volumes[metric]); }) 
+        .style("stroke-width", function(d, i) { return volumePaletteWidth(d.volumes[metric]); });
+    chartApi.txt
+        .text(function(d, i) { return d.volumes[metric].toLocaleString(); });    
+} // symbolizeViz()
